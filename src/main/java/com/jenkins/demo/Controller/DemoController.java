69d45f9ed4cad31ea82f668aacc1a1be3b3efec1
wubixuan <wubixuan@qq.com>
2023-05-05 09:56:09 +0800

    first commit

---
+++ src/main/java/com/jenkins/demo/Controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.jenkins.demo.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world!";
+    }
+}
